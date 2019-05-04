f18e4c9e6ad51ed4e7180ea398a43d5f00880225
Pragathi Madushinie <prmadushini@gmail.com>
2019-05-03 22:36:57 -0500

    readme

---
+++ PyBank/main.py
@@ -0,0 +1,97 @@
+#libraries export 
+import os
+import csv
+
+#tot_months as Str
+#tot_pl as Double
+
+#as the csv is in the loxal folder
+budget_data = os.path.join("budget_data.csv")
+
+#variables defined
+tot_months = 0
+tot_pl = 0
+val = 0
+change = 0
+
+#arrays defined
+dates = []
+profits = []
+
+
+with open(budget_data, newline = "") as csvfile:
+    csvreader = csv.reader(csvfile, delimiter = ",")
+
+   
+    csv_header = next(csvreader)
+
+    #first row
+    first_row = next(csvreader)
+    tot_months = tot_months + 1
+    tot_pl = tot_pl  + int(first_row[1])
+    val = int(first_row[1])
+    
+    #going through all the rows
+    for row in csvreader:
+        
+        dates.append(row[0])
+        
+        
+        change = int(row[1])-val
+        #added it to the profits array i created
+        profits.append(change)
+        val = int(row[1])
+        
+        
+        tot_months = tot_months + 1
+
+        
+        tot_pl = tot_pl + int(row[1])
+
+
+    #The average of the changes in "Profit/Losses" over the entire period
+    avg_change = sum(profits)/len(profits)
+
+    #The greatest increase in profits (date and amount) over the entire period
+    greatest_increase = max(profits)
+    greatest_index = profits.index(greatest_increase)
+    greatest_date = dates[greatest_index]
+
+    #The greatest decrease in losses (date and amount) over the entire period
+    greatest_decrease = min(profits)
+    worst_index = profits.index(greatest_decrease)
+    worst_date = dates[worst_index]
+
+    
+    
+    
+
+#As an example, your analysis should look similar to the one below:
+print("'''text")
+print("Financial Analysis")
+print("---------------------")
+#The total number of months included in the dataset
+print(f"Total Months: {str(tot_months)}")
+#The net total amount of "Profit/Losses" over the entire period
+print(f"Total: ${str(tot_pl)}")
+#The average of the changes in "Profit/Losses" over the entire period
+print(f"Average Change: ${str(round(avg_change,2))}")
+#The greatest increase in profits (date and amount) over the entire period
+print(f"Greatest Increase in Profits: {greatest_date} (${str(greatest_increase)})")
+#The greatest decrease in losses (date and amount) over the entire period
+print(f"Greatest Decrease in Profits: {worst_date} (${str(greatest_decrease)})")
+print("'''")
+
+#print the analysis to the terminal and export a text file with the results.
+output = open("PyBankOutput.txt", "w")
+
+line1 = "'''text"
+line2 = "Financial Analysis"
+line3 = "---------------------"
+line4 = str(f"Total Months: {str(tot_months)}")
+line5 = str(f"Total: ${str(tot_pl)}")
+line6 = str(f"Average Change: ${str(round(avg_change,2))}")
+line7 = str(f"Greatest Increase in Profits: {greatest_date} (${str(greatest_increase)})")
+line8 = str(f"Greatest Decrease in Profits: {worst_date} (${str(greatest_decrease)})")
+line9 = "'''"
+output.write('{}\n{}\n{}\n{}\n{}\n{}\n{}\n{}\n{}\n'.format(line1,line2,line3,line4,line5,line6,line7,line8,line9))
